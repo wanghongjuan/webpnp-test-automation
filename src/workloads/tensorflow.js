821e0ef87782a8bedda75221eb2bf897a0c4e887
cancaox <canx.cao@intel.com>
2020-12-10 14:05:37 +0800

    Support TensorFlow SIMD on Windows and Linux

--- src/workloads/tensorflow.js
+++ src/workloads/tensorflow.js
@@ -8,7 +8,7 @@ async function runTensorflowTest(workload, flags) {
   let args = ["--start-maximized"];
   if(workload.name === "TensorFlow_Simd"){
     let simdFlag = "--enable-feature=WebAssemblySimd,WebAssemblyThreads";
-    flags.push(simdFlag);
+    args.push(simdFlag);
   }
   if (flags !== undefined) {
     args = args.concat(flags);
@@ -77,8 +77,6 @@ async function runTensorflowTest(workload, flags) {
   console.log(scores);
 
   await browser.close();
-
-  flags.pop();
 
   return Promise.resolve({
     date: Date(),
